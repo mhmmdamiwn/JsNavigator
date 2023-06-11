7b1b483e349bd0ee57c7f33ae2a81f2b8953a815
MmD <mmdj27634@gmail.com>
2023-06-11 16:15:05 +0330

    add : button element

---
+++ client/components/ui/button.jsx
@@ -0,0 +1,22 @@
+import { cn } from "../../helpers/cn";
+
+function Button({ size = "md", block = false, children, className, ...props }) {
+  const sizes = {
+    md: "h-[38px] text-[13px]",
+  };
+  return (
+    <button
+      className={cn(
+        "flex items-center justify-center bg-violet-500 hover:bg-violet-500/90 px-4 text-white rounded",
+        sizes[size],
+        block ? "w-full" : "",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
