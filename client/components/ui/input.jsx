90f97a0f752bbb80d9d7148673a135b30a2cd50f
MmD <mmdj27634@gmail.com>
2023-06-13 00:43:05 -0700

    add : removed ui folder with its components

---
+++ client/components/ui/input.jsx
@@ -0,0 +1,35 @@
+import { useId } from "preact/hooks";
+import { cn } from "../../helpers/cn";
+
+const Input = ({ label, error, value, setValue }) => {
+  const id = useId();
+  return (
+    <div className="flex flex-col gap-2 w-full relative">
+      {label ? (
+        <label className="text-[13px] text-white w-fit" htmlFor={id}>
+          {label}
+        </label>
+      ) : (
+        ""
+      )}
+      <div
+        className={cn(
+          "flex items-center bg-neutral-800 gap-1 text-white border border-white/25 text-[13px] rounded p-2.5 focus-within:shadow-[0_0_0_2px] focus-within:shadow-white/5 focus-within:border-violet-400",
+          error ? "border-red-400" : ""
+        )}
+      >
+        <input
+          value={value}
+          onInput={(e) => {
+            setValue(e.target.value);
+          }}
+          autoComplete="off"
+          className={cn("outline-none bg-transparent w-full", "")}
+          id={id}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Input;
--- main.js
+++ main.js
@@ -1,39 +1,39 @@
-const http = require('http');
-const JsNavigator = require('./JsNavigator')
-const getFilesInDependencyOrder = require('./GetFileInDependencyOrder')
-const { URL } = require('url');
-const querystring = require('querystring');
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
+const http = require("http");
+const JsNavigator = require("./JsNavigator");
+const getFilesInDependencyOrder = require("./GetFileInDependencyOrder");
+const { URL } = require("url");
+const querystring = require("querystring");
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 const server = http.createServer((request, response) => {
-
   const { method, url } = request;
   const parsedUrl = new URL(url, `http://${request.headers.host}`);
-    const queryParams = querystring.parse(parsedUrl.searchParams.toString());
+  const queryParams = querystring.parse(parsedUrl.searchParams.toString());
 
-    if(queryParams)
-  JsNavigator(queryParams.entry);
-  const dependencyOrder = getFilesInDependencyOrder(`${process.cwd()}/${queryParams.entry}`, queryParams.entry);
+  if (queryParams) JsNavigator(queryParams.entry);
+  const dependencyOrder = getFilesInDependencyOrder(
+    `${process.cwd()}/${queryParams.entry}`,
+    queryParams.entry
+  );
   console.log(dependencyOrder);
-  if (method === 'GET' && parsedUrl.pathname === '/jsnavigator') {
-    response.setHeader('Access-Control-Allow-Origin', 'http://localhost:5174'); // Allow requests from http://localhost:5174
-    response.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-    response.setHeader('Access-Control-Allow-Credentials', 'true')
-response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-response.setHeader('Content-Type', 'application/json'); // Update content type to application/json
+  if (method === "GET" && parsedUrl.pathname === "/jsnavigator") {
+    response.setHeader("Access-Control-Allow-Origin", "http://localhost:5174"); // Allow requests from http://localhost:5174
+    response.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    response.setHeader("Access-Control-Allow-Credentials", "true");
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    response.setHeader("Content-Type", "application/json"); // Update content type to application/json
 
-response.writeHead(200);
-      response.end(JSON.stringify(dependencyOrder));
+    response.writeHead(200);
+    response.end(JSON.stringify(dependencyOrder));
   } else {
     // Handle other routes
-    response.writeHead(404, { 'Content-Type': 'text/plain' });
-    response.end('Not Found');
+    response.writeHead(404, { "Content-Type": "text/plain" });
+    response.end("Not Found");
   }
 });
 
