150d5095364535f62371152a1dea08002596d29d
Amin <amin@Amins-MacBook-Pro.local>
2023-06-24 01:05:51 +0330

    changed logs.txt input for jsnavigator.js

--- JsNavigator.js
+++ JsNavigator.js
@@ -5,7 +5,7 @@ var originalRequire = Module.prototype.require;
 function JsNavigator(){
    Module.prototype.require = function(arg){
     const resolvedPath = require.resolve(arg);
-    const fileContent = fs.readFileSync(__dirname+'/Logs.txt', 'utf-8');
+    const fileContent = fs.readFileSync('./Logs.txt', 'utf-8');
     fs.writeFileSync('./Logs.txt',fileContent+require.resolve(arg).replace(process.cwd(),'root')+'\n','utf-8')
     delete require.cache[startFile];
     delete require.cache[resolvedPath];
