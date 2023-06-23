eccd24ceb8672cf2a124fa961d0d5cc2893e82b9
Amin <amin@Amins-MacBook-Pro.local>
2023-06-24 01:03:11 +0330

    final version 2

--- JsNavigator.js
+++ JsNavigator.js
@@ -3,17 +3,13 @@ const fs = require('fs')
 var Module = require('module');
 var originalRequire = Module.prototype.require;
 function JsNavigator(){
-    
-  
-}
-function addLogger(startFile){
-  Module.prototype.require = function(arg){
+   Module.prototype.require = function(arg){
     const resolvedPath = require.resolve(arg);
-    const fileContent = fs.readFileSync(process.cwd()+'/Logs.txt', 'utf-8');
+    const fileContent = fs.readFileSync(__dirname+'/Logs.txt', 'utf-8');
     fs.writeFileSync('./Logs.txt',fileContent+require.resolve(arg).replace(process.cwd(),'root')+'\n','utf-8')
     delete require.cache[startFile];
     delete require.cache[resolvedPath];
     return originalRequire.apply(this, arguments);
     };
 }
-module.exports = {addLogger,JsNavigator};
+module.exports = {JsNavigator};
--- main.js
+++ main.js
@@ -5,33 +5,32 @@ const findExecutedPath = require('./findExecutedPath')
 const app = require('express')()
 
 
-  let dependencyOrder;
+let dependencyOrder;
 
-app.get('/jsnavigator',(req,res)=>{
-    const queryParams = req.query;
-   dependencyOrder = getFilesInDependencyOrder(`${process.cwd()}/${queryParams.entry}`,queryParams.importMethod);
-  console.log(dependencyOrder);
-  res.set("Access-Control-Allow-Origin", "*"); 
+app.get('/jsnavigator', (req, res) => {
+  const queryParams = req.query;
+  dependencyOrder = getFilesInDependencyOrder(`${process.cwd()}/${queryParams.entry}`, queryParams.importMethod);
+  res.set("Access-Control-Allow-Origin", "*");
   res.set("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.set("Access-Control-Allow-Credentials", "true");
-  res.set("Content-Type", "application/json"); 
-    res.send(JSON.stringify(dependencyOrder));
-  
+  res.set("Content-Type", "application/json");
+  res.send(JSON.stringify(dependencyOrder));
+
 })
 
-app.get('/jsnavigator/postman',(req,res)=>{
-  const lastExecutedFile = logReader("./Logs.txt",dependencyOrder)
-  const executedPath = findExecutedPath(lastExecutedFile,dependencyOrder);
-  res.set("Access-Control-Allow-Origin", "*"); 
+app.get('/jsnavigator/postman', (req, res) => {
+  const lastExecutedFile = logReader("./Logs.txt", dependencyOrder)
+  const executedPath = { "executed": findExecutedPath(lastExecutedFile, dependencyOrder).reverse() };
+  res.set("Access-Control-Allow-Origin", "*");
   res.set("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.set("Access-Control-Allow-Credentials", "true");
-  res.set("Content-Type", "application/json"); 
-    res.send(JSON.stringify(executedPath));
-  }
+  res.set("Content-Type", "application/json");
+  res.send(JSON.stringify(executedPath));
+}
 )
 
-  app.listen(8585,(err)=>{
-    if(err)
+app.listen(8585, (err) => {
+  if (err)
     console.log(err)
-    console.log('server listening on port 8585')
-  })
+  console.log('server listening on port 8585')
+})
