7da60529bfe2165a82614cb73d81a4542a433fbb
Amin <amin@Amins-MacBook-Pro.local>
2023-06-24 01:16:32 +0330

    fixed some path to files

--- JsNavigator.js
+++ JsNavigator.js
@@ -5,11 +5,12 @@ var originalRequire = Module.prototype.require;
 function JsNavigator(){
    Module.prototype.require = function(arg){
     const resolvedPath = require.resolve(arg);
-    const fileContent = fs.readFileSync('./Logs.txt', 'utf-8');
-    fs.writeFileSync('./Logs.txt',fileContent+require.resolve(arg).replace(process.cwd(),'root')+'\n','utf-8')
+    const fileContent = fs.readFileSync(process.cwd()+'/node_modules/js-navigator/Logs.txt', 'utf-8');
+    fs.writeFileSync(process.cwd()+'/node_modules/js-navigator/Logs.txt',fileContent+require.resolve(arg).replace(process.cwd(),'root')+'\n','utf-8')
     delete require.cache[startFile];
     delete require.cache[resolvedPath];
     return originalRequire.apply(this, arguments);
     };
+   
 }
 module.exports = {JsNavigator};
