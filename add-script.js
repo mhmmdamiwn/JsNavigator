50b231f96504757d5ae1eb64036cc185b0561703
Amin <amin@Amins-MacBook-Pro.local>
2023-06-26 13:11:34 +0330

    fix package.json 4

--- add-script.js
+++ add-script.js
@@ -3,7 +3,8 @@ const path = require('path');
 
 const pkgPath = path.resolve(`${process.cwd()}/package.json`);
 const pkg = require(pkgPath);
-
+console.log('path',pkgPath)
+console.log('pkg',pkg)
 pkg.scripts.nav = 'vite && node main.js';
 
 fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
