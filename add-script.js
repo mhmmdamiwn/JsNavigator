de795389d725d59fd40b46e0c0509cdcb63190c2
MmD <mmdj27634@gmail.com>
2023-06-27 03:22:40 -0700

    add : concurrenlty to script of package so it can run both front and back end at same terminal

--- add-script.js
+++ add-script.js
@@ -1,10 +1,12 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const pkgPath = path.resolve('../../package.json');
+const pkgPath = path.resolve("../../package.json");
 const pkg = require(pkgPath);
-console.log('path',pkgPath)
-console.log('pkg',pkg)
-pkg.scripts.nav = 'vite && node main.js';
+
+console.log("path", pkgPath);
+console.log("pkg", pkg);
+
+pkg.scripts["js-nav"] = "(cd node_modules/js-navigator && npm run js-nav)";
 
 fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
