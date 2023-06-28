c3f7bb7266349ea91f530bae15a43e674be1a133
Amin <amin@Amins-MacBook-Pro.local>
2023-06-28 14:36:31 +0330

    try to fix require packages try 3

--- JsNavigator.js
+++ JsNavigator.js
@@ -6,7 +6,7 @@ var originalRequire = Module.prototype.require;
 function JsNavigator(startFile){
    Module.prototype.require = function(arg){
     
-    if(path.isAbsolute(arg) || arg.includes('.')){
+    if(path.isAbsolute(arg) || arg.includes('.') || arg.includes('/')){
         const resolvedPath = require.resolve(path.resolve(arg));
         const fileContent = fs.readFileSync(process.cwd()+'/node_modules/js-navigator/Logs.txt', 'utf-8');
         fs.writeFileSync(process.cwd()+'/node_modules/js-navigator/Logs.txt',fileContent+require.resolve(path.resolve(arg)).replace(process.cwd(),'root')+'\n','utf-8')
