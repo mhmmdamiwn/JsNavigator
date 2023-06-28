b05cc40f185a3a7bdf6169a777783e6e98ab7f21
Amin <amin@Amins-MacBook-Pro.local>
2023-06-28 12:27:35 +0330

    try to fix requier packages 2

--- JsNavigator.js
+++ JsNavigator.js
@@ -5,11 +5,14 @@ const path = require('path')
 var originalRequire = Module.prototype.require;
 function JsNavigator(startFile){
    Module.prototype.require = function(arg){
-    const resolvedPath = require.resolve(path.resolve(arg));
-    const fileContent = fs.readFileSync(process.cwd()+'/node_modules/js-navigator/Logs.txt', 'utf-8');
-    fs.writeFileSync(process.cwd()+'/node_modules/js-navigator/Logs.txt',fileContent+require.resolve(path.resolve(arg)).replace(process.cwd(),'root')+'\n','utf-8')
-    delete require.cache[startFile];
-    delete require.cache[resolvedPath];
+    
+    if(path.isAbsolute(arg) || arg.includes('.')){
+        const resolvedPath = require.resolve(path.resolve(arg));
+        const fileContent = fs.readFileSync(process.cwd()+'/node_modules/js-navigator/Logs.txt', 'utf-8');
+        fs.writeFileSync(process.cwd()+'/node_modules/js-navigator/Logs.txt',fileContent+require.resolve(path.resolve(arg)).replace(process.cwd(),'root')+'\n','utf-8')
+        delete require.cache[startFile];
+        delete require.cache[resolvedPath];
+    }
     return originalRequire.apply(this, arguments);
     };
    
